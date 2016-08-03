051a06b4d6cecbd850f17738d3bf4aa85038275e
Juanfer Sanchez <juanfersanchezv@gmail.com>
2016-08-03 10:41:54 -0600

    portafolio para JC

---
+++ index.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Portafolio Informatico</title>
+
+		<!-- Bootstrap -->
+		<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+		<link href="css/main.css" rel="stylesheet">
+		<link rel="shortcut icon" href="images/favicon.png" type="image/png">
+
+	</head>
+	<body>
+		<div class="page-header">
+		<img style ="height: auto; max-width: 400px;" src="images/logo_ticsolution.png" alt="">
+			<h1><div><small>Portafolio de Sistemas Informáticos</small></div></h1>
+		</div>
+		<section id="contenedor">
+			<div class="row">
+				<div class="col-md-4">
+					<div class="boton-panel">
+						<a href="http://sistemamacesa.com/backup/sistema/" target="_blank" class="link">
+							<p class="titulo">Sistema de Backup</p>
+							<p>Sistema que almacena fotografias, documentos, puntos geograficos, boletas de campo, para diferentes estudios de avaluos bancarios, comerciales, cada avaluo se puede ver geograficamente con sus diferentes filtos de buscquedad.</p>
+						</a>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="boton-panel">
+						<a href="http://sistemamacesa.com/backup/sistema/index.php?r=Administrar/admin" target="_blank" class="link">
+							
+							<p class="titulo">Dashboard Mensual Backup</p>
+							<p>Tablero informativo para la visualizacion de informacion del sistema backup, Informacion: Ultimos usuarios en logiarse, cantidad de avaluos del mes,  Filtros de actividades, Rendimiento global del servidor.</p>
+						</a>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="boton-panel">
+						<a href=" http://mapgeo.com.gt/simic/" target="_blank" class="link">
+							
+							<p class="titulo">SIMIC</p>
+							<p><b>Sistema de cuencas y monitoreo de indicadores</b></p>
+							<p>Sistema capaz  de recolectar estudios economicos, sociales, naturales con respecto a las diferentes cuencas y microcuencas del pais, Esta version esta para las cuencas del lago de Amatitlán, se puede ver los mapas y donde se realizaron los diferentes estudios.</p>
+						</a>
+					</div>
+				</div>
+				
+			</div>
+
+			<div class="row">
+				
+				<div class="col-md-4">
+					<div class="boton-panel">
+						<a href="http://mapgeo.com.gt/" target="_blank" class="link">
+							<p class="titulo">Mapa Centro América</p>
+							<p> Ejemplo de cómo visualizar el mapa de centro america, con sus repectivos estudios, usos de suelo, y cambio de temperatura.</p>
+						</a>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="boton-panel">
+						<a href="http://mingob.gob.gt/240" target="_blank" class="link">
+							<p class="titulo">Boletas en Línea</p>
+							<p>Sistema que permite llevar el control de los creditos basados en regiones, tambien cuenta con modulos para grafíco por region y sucursal, ademas de llevar los procesos en tiempo real. </p>
+						</a>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="boton-panel">
+						<a href=" http://developersmacesa.com/macesa/index.php?r=site/login" target="_blank" class="link">
+							
+							<p class="titulo">Documentos en línea</p>
+							<p>Sistema para generar formatos informes en pdf, con una estrutura dinamica, para minimizar los errores de los digitadores. Esta herramienta se desarrolla dependiendo las necesidades de los clientes.</p>
+						</a>
+					</div>
+				</div>
+				
+			</div>
+
+			<!-- <div class="row">
+				
+				<div class="col-md-4">
+					<div class="boton-panel">
+						<a href="http://aplicaciones.mingob.gob.gt/puerta" target="_blank" class="link">
+							
+							<p class="titulo">Registro de Visitantes</p>
+							<p>Aplicación utilizada para llevar el control de los visitantes que ingresan al MINGOB</p>
+						</a>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="boton-panel">
+						<a href="http://sipol.mingob.gob.gt/sipol/" target="_blank" class="link">
+							
+							<p class="titulo">SIPOL</p>
+							<p>Sistema utilizado en las comisarias de la PNC para poder tomar denuncias, extravios , incidencia y consignacion</p>
+						</a>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="boton-panel">
+						<a href="http://sistemas.mingob.gob.gt/sms-admin/" target="_blank" class="link">
+							
+							<p class="titulo">SMS Admin</p>
+							<p>Sistema para administrar los numeros de telefono el cual tendran la opcion de consultar solvecias de personas por DPI y por numero de placa</p>
+						</a>
+					</div>
+				</div>
+				
+			</div> -->
+
+	<!-- 		<div class="row">
+				
+				<div class="col-md-4">
+					<div class="boton-panel">
+						<a href="https://sistemas.mingob.gob.gt/sui/" target="_blank" class="link">
+							
+							<p class="titulo">SUI</p>
+							<p>La aplicaci&oacuten est&aacute enfocada para que un investigador en una sola aplicaci&oacuten, pueda consultar m&uacuteltiples bases de datos, siendo estas: RENAP, antecedentes policiales, ordenes de captura, flujo migratorio, pasaportes, licencias, arraigos, veh&iacuteculos. </p>
+						</a>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="boton-panel">
+						<a href="http://sistemas.mingob.gob.gt/suiPatrullas/" target="_blank" class="link">
+							
+							<p class="titulo">SUI Patrullas</p>
+							<p>Sistema pensado para el uso exclusivo de las patrullas de la Policia Nacional Civil, el cual tienen acceso a consultar solvecias de ciudadanos y vehiculos</p>
+						</a>
+					</div>
+				</div>
+				
+			</div> -->
+
+		</section>
+
+		<footer class="site-footer">
+			<div class="imagenes_grises">
+				<span> 
+						<a href="http://ticsolution.com.gt/gt/" target="_blank">
+							<img title="" src="images/logo_ticsolution.png" width="100" height="50"data-original-title="Ticsolution"></a></span><a href="http://ticsolution.com.gt/gt/" target="_blank"> 
+						</a>
+			</div>
+			Tºodos los derechos reservados 2016 ©
+		</footer>
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+		<!-- Include all compiled plugins (below), or include individual files as needed -->
+		<script src="bootstrap/js/bootstrap.min.js"></script>
+		</body>
+</html>
+
