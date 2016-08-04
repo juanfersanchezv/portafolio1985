3048ad76a7f8f7a08be1e45e02e5e916da768c1e
Juanfer Sanchez <juanfersanchezv@gmail.com>
2016-08-04 09:35:27 -0600

    cambio de color en la vista, y en css

--- index.php
+++ index.php
@@ -139,7 +139,7 @@
 			<div class="">
 				<span> 
 						<a href="http://ticsolution.com.gt/gt/" target="_blank">
-							<img title="" src="images/logo_ticsolution.png" width="100" height="50"data-original-title="Ticsolution"></a></span><a href="http://ticsolution.com.gt/gt/" target="_blank"> 
+							<img title="" src="images/logo_ticsolution.png" width="20%" data-original-title="Ticsolution"></a></span><a href="http://ticsolution.com.gt/gt/" target="_blank"> 
 						</a>
 			</div>
 			
